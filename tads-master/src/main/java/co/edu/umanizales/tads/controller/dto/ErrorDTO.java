16766d523a6d9c4e68608aa5d5c5494f2344faf6
WAYNE120804 <127144981+WAYNE120804@users.noreply.github.com>
2023-05-06 14:11:45 -0500

    control advice

--- tads-master/src/main/java/co/edu/umanizales/tads/controller/dto/ErrorDTO.java
+++ tads-master/src/main/java/co/edu/umanizales/tads/controller/dto/ErrorDTO.java
@@ -1,10 +1,17 @@
 package co.edu.umanizales.tads.controller.dto;
 
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@Builder
+
 public class ErrorDTO {
     private int code;
     private String message;
 
+    public ErrorDTO(int code, String message) {
+        this.code = code;
+        this.message = message;
+    }
 }
